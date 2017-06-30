55ecf4673316cde23c64843edf1517c996ab812a
jessie <junchangchen@gmail.com>
2017-06-30 11:24:48 +0800

    index

---
+++ src/tubesite/static/tubesite/images/a.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for i in {1..9}; do
+    wget http://divothemes.com/cutetube/images/photo_0$i.jpg
+done
+    
+for i in {10..25}; do
+    wget http://divothemes.com/cutetube/images/photo_$i.jpg
+done
+
+wget http://divothemes.com/cutetube/images/slide_02.jpg
+wget http://divothemes.com/cutetube/images/slide_03.jpg
+wget http://divothemes.com/cutetube/images/slide_04.jpg

2b32d9158403b18ab2fedebe080f010ff12626f5
jessie <junchangchen@gmail.com>
2017-07-02 13:06:10 +0800

    model

--- src/tubesite/admin.py
+++ src/tubesite/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from models import Category, Video
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Video)
--- src/tubesite/models.py
+++ src/tubesite/models.py
@@ -4,3 +4,22 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="category")
+
+
+class Video(models.Model):
+    QUALITY_CHOICES = (
+        (0, 'Normal'),
+        (1, 'HD'),
+    )
+    name = models.CharField(max_length=100)
+    desc = models.TextField(max_length=200)
+    poster = models.URLField()
+    src = models.URLField()
+    duration = models.IntegerField()
+    quality = models.IntegerField(choices=QUALITY_CHOICES, default=0)
+
